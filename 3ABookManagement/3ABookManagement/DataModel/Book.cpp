8f421cb65289b751881ad00a1bb8e1fc7c749a99
gchen <Guoliang.Chen@netbraintech.com>
2021-04-15 22:45:54 -0400

    3. Upload the DataModel - Book

--- 3ABookManagement/3ABookManagement/DataModel/Book.cpp
+++ 3ABookManagement/3ABookManagement/DataModel/Book.cpp
@@ -3,10 +3,98 @@
 
 
 CBook::CBook()
+: m_strID("")
+, m_strName("")
+, m_strNote("")
+, m_strPublish("")
+, m_strMemberID("")
+, m_dCost(0.0)
+, m_dPrice(0.0)
+, m_dProfit(0.0)
+, m_dDiscount(0.0)
+, m_dOverAmount(0.0)
+, m_tmSale(NULL)
+, m_tmBorrow(NULL)
+, m_tmReturn(NULL)
+, m_tmOverBorrow(NULL)
 {
 }
 
-
 CBook::~CBook()
 {
+}
+
+void CBook::SetBookType(int nType)
+{
+	m_nType = nType;
+}
+
+void CBook::SetBookID(const string & strID)
+{
+	m_strID = strID;
+}
+
+void CBook::SetBookName(const string & strName)
+{
+	m_strName = strName;
+}
+
+void CBook::SetBookNote(const string & strNote)
+{
+	m_strNote = strNote;
+}
+
+void CBook::SetBookPublish(const string & strPublish)
+{
+	m_strPublish = strPublish;
+}
+
+void CBook::SetBookMemberID(const string & strMemberID)
+{
+	m_strMemberID = strMemberID;
+}
+
+void CBook::SetBookCost(double dCost)
+{
+	m_dCost = dCost;
+}
+
+void CBook::SetBookPrice(double dPrice)
+{
+	m_dPrice = dPrice;
+}
+
+void CBook::SetBookProfit(double dProfit)
+{
+	m_dProfit = dProfit;
+}
+
+void CBook::SetBookDiscount(double dDiscount)
+{
+	m_dDiscount = dDiscount;
+}
+
+void CBook::SetBookOverAmount(double dOverAmount)
+{
+	m_dOverAmount = dOverAmount;
+}
+
+void CBook::SetBookSaleTime(const time_t & tmSale)
+{
+	m_tmSale = tmSale;
+}
+
+void CBook::SetBookBorrowTime(const time_t & tmBorrow)
+{
+	m_tmBorrow = tmBorrow;
+}
+
+void CBook::SetBookReturnTime(const time_t & tmReturn)
+{
+	m_tmReturn = tmReturn;
+}
+
+void CBook::SetBookOverBorrowTime(const time_t & tmOverBorrow)
+{
+	m_tmOverBorrow = tmOverBorrow;
 }
--- 3ABookManagement/3ABookManagement/DataModel/Book.h
+++ 3ABookManagement/3ABookManagement/DataModel/Book.h
@@ -1,8 +1,63 @@
 #pragma once
+#include <ctime>
+#include "string"
+
+using namespace std;
+
 class CBook
 {
 public:
 	CBook();
 	~CBook();
+
+public:
+	int GetBookType() { return m_nType; }
+	string GetBookID() { return m_strID; }
+	string GetBookName() { return m_strName; }
+	string GetBookNote() { return m_strNote; }
+	string GetBookPublish() { return m_strPublish; }
+	string GetBookMemberID() { return m_strMemberID; }
+	double GetBookCost() { return m_dCost; }
+	double GetBookPrice() { return m_dPrice; }
+	double GetBookProfit() { return m_dProfit; }
+	double GetBookDiscount() { return m_dDiscount; }
+	double GetBookOverAmount() { return m_dOverAmount; }
+	time_t GetBookSaleTime() { return m_tmSale; }
+	time_t GetBookBorrowTime() { return m_tmBorrow; }
+	time_t GetBookReturnTime() { return m_tmReturn; }
+	time_t GetBookOverBorrowTime() { return m_tmOverBorrow; }
+
+	void SetBookType(int nType);
+	void SetBookID(const string & strID);
+	void SetBookName(const string & strName);
+	void SetBookNote(const string & strNote);
+	void SetBookPublish(const string & strPublish);
+	void SetBookMemberID(const string & strMemberID);
+	void SetBookCost(double dCost);
+	void SetBookPrice(double dPrice);
+	void SetBookProfit(double dProfit);
+	void SetBookDiscount(double dDiscount);
+	void SetBookOverAmount(double dOverAmount);
+	void SetBookSaleTime(const time_t & tmSale);
+	void SetBookBorrowTime(const time_t & tmBorrow);
+	void SetBookReturnTime(const time_t & tmReturn);
+	void SetBookOverBorrowTime(const time_t & tmOverBorrow);
+
+private:
+	int m_nType;
+	string m_strID;
+	string m_strName;
+	string m_strNote;
+	string m_strPublish;
+	string m_strMemberID;
+	double m_dCost;
+	double m_dPrice;
+	double m_dProfit;
+	double m_dDiscount;
+	double m_dOverAmount;
+	time_t m_tmSale;
+	time_t m_tmBorrow;
+	time_t m_tmReturn;
+	time_t m_tmOverBorrow;
 };
 
